2cdd6ceec784f65b5334ea9c1893fb12e8b76044
gmlincoln <maula03@gmail.com>
2023-08-23 13:54:16 +0600

    added-some-features

--- js/app.js
+++ js/app.js
@@ -13,7 +13,17 @@ function generateQrCode() {
         showQr.innerHTML = `
             <p>Your QR Code</p>
             <img src="${qrValue}"/>
+            <button
+            id="download-btn"
+            type="button"
+            class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            download
+            >
+            Download
+          </button>
             `;
+
+
         qrInputFieldStr.value = ' ';
     }
 }
