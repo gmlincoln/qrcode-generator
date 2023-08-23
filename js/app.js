9ae10776c1ab8ed6ccb08c79ad1f6bf755c76cbf
gmlincoln <maula03@gmail.com>
2023-08-23 15:23:41 +0600

    added-some-features

--- js/app.js
+++ js/app.js
@@ -13,14 +13,13 @@ function generateQrCode() {
         showQr.innerHTML = `
             <p>Your QR Code</p>
             <img src="${qrValue}"/>
-            <button
+            <a
             id="download-btn"
-            type="button"
-            class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            href="${qrValue}"
             download
             >
-            Download
-          </button>
+            <button class="text-white cursor-pointer bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Download </button>
+          </a>
             `;
 
 
